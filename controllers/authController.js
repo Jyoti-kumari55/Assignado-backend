a2aa849f89f71491df2d13d94257d99e09cc651e
jyoti-ch15 <jytoich15.edudite@gmail.com>
2025-06-30 12:42:31 +0530

    ..

--- controllers/authController.js
+++ controllers/authController.js
@@ -93,8 +93,8 @@ const loginUser = async (req, res) => {
     );
     res.cookie("token", token, {
       httpOnly: true,
-      secure: true,
-      sameSite: "None",
+      secure: false,
+      // sameSite: "None",
       maxAge: 8 * 60 * 60 * 1000,
     });
 
