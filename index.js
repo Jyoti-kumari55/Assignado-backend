bb7965ff612c56803c66a84b9430a0b36105af3b
jyoti-ch15 <jytoich15.edudite@gmail.com>
2025-07-13 23:09:45 +0530

    added frontend url

--- index.js
+++ index.js
@@ -21,8 +21,18 @@ const upload = require("./middlewares/uploader");
 
 const app = express();
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://assignado-app.vercel.app",
+];
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("CORS not allowed"), false);
+    }
+  },
   credentials: true,
   optionSuccessStatus: 200,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
